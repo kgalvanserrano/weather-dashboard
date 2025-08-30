c14111a6612ce976a6b2711380f5ff35abe07814
kgalvanserrano <kevin.galvan@live.com>
2025-08-29 17:31:57 -0700

    add input validation and return result in getData function

--- src/services/weatherAPI.js
+++ src/services/weatherAPI.js
@@ -1,4 +1,7 @@
 async function getData(lat, lon, time) {
+    if (lat === null || lon === null || time === null) { // input validation, making sure args are actually usable b4 making API call
+      throw new Error("Missing parameters");
+    }
   const url = `https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${time}&appid=${import.meta.env.WEATHER_API_KEY}`;
   try {
     const response = await fetch(url);
@@ -8,6 +11,7 @@ async function getData(lat, lon, time) {
 
     const result = await response.json();
     console.log(result);
+    return result;
   } catch (error) {
     console.error(error.message);
   }
